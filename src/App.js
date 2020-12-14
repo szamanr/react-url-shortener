5d0888bd7ff4d254469772bbb32d73dc187e05db
rrrafalsz <rrrafalsz@gmail.com>
2020-12-14 11:06:18 +0100

    move browser router to app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import {Route} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import './App.css';
 import Shortener from "./Shortener";
 import Resolver from "./Resolver";
 
 function App() {
     return (
-        <div className="App">
-            <Route exact path="/">
-                <Shortener/>
-            </Route>
+        <BrowserRouter>
+            <div className="App">
+                <Route exact path="/">
+                    <Shortener/>
+                </Route>
 
-            <Route path="/:slug" component={Resolver}>
-            </Route>
-        </div>
+                <Route path="/:slug" component={Resolver}>
+                </Route>
+            </div>
+        </BrowserRouter>
     );
 }
 
