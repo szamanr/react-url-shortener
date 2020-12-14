929614a52c146d55cbc6ebc5c8911cbbe755c694
rrrafalsz <rrrafalsz@gmail.com>
2020-12-14 11:31:39 +0100

    implement tests for app routing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders shortener', () => {
+    render(<App/>);
+    const pageTitle = screen.getByText(/URL Shortener/i);
+    expect(pageTitle).toBeInTheDocument();
+});
+
+test('renders resolver', () => {
+    window.history.pushState({}, 'test page', '/random-slug');
+    render(<App/>, {wrapper: BrowserRouter});
+
+    const redirectionText = screen.getByText(/Redirecting/i);
+    expect(redirectionText).toBeInTheDocument();
 });
